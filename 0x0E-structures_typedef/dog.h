617fc383e08cb8a040537d398d9f6e6610883cf9
Ebuka500 <Ebuka500.e.eze46@gmail.com>
2023-01-16 19:24:22 -0800

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -27,3 +27,23 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif /* struct dog */
+
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	dog_t my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}
